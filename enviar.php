0f8424bc029c1a1439cbdc5a6c1520b31c224a5d
mvnigro <mvnigro@gmail.com>
2022-06-19 15:22:24 -0300

    ok

--- enviar.php
+++ enviar.php
@@ -6,9 +6,8 @@
     $product=$_POST['product'];
     $moq=$_POST['moq'];
     $etd=$_POST['etd'];
-    $photo=$_POST['photo'];
     $price=$_POST['price'];
- 
+   
 
     $mensagem= 'Esta mensagem foi enviada através do formulário<br><br>';
     $mensagem.='<b>Nome da Empresa: </b>'.$empresa.'<br>';
@@ -18,7 +17,6 @@
     $mensagem.='<b>Produto:</b> '.$product.'<br>';
     $mensagem.='<b>MOQ:</b> '.$moq.'<br>';
     $mensagem.='<b>ETD:</b> '.$etd.'<br>';
-    $mensagem.='<b>Photo:</b> '.$photo.'<br>';
     $mensagem.='<b>Preço:</b> '.$price.'<br>';
     require("PHPMailer-master/src/PHPMailer.php");
     require("PHPMailer-master/src/SMTP.php");
@@ -29,15 +27,15 @@ $mail = new PHPMailer\PHPMailer\PHPMailer();
     $mail->SMTPDebug = 2;	
     $mail->Host       = 'smtp.hostinger.com';  // SEU HOST (HOSPEDAGEM)
     $mail->SMTPAuth   = true;                        // Manter em true
-    $mail->Username   = 'vicente@vnigro.net';   //SEU USUÁRIO DE EMAIL
-    $mail->Password   = 'Vicente156@@##';                   //SUA SENHA DO EMAIL SMTP password
+    $mail->Username   = '${{ secrets.USUARIO_EMAIL }}';   //SEU USUÁRIO DE EMAIL
+    $mail->Password   = '${{ secrets.USUARIO_EMAIL_SENHA }}';                   //SUA SENHA DO EMAIL SMTP password
     $mail->SMTPSecure = 'tls';    //TLS OU SSL-VERIFICAR COM A HOSPEDAGEM
     $mail->Port       = 587;     //TCP PORT, VERIFICAR COM A HOSPEDAGEM
     $mail->CharSet = 'UTF-8';
   
     
     //Recipients
-    $mail->setFrom('vicente@vnigro.net', 'Quote from Site');  //DEVE SER O MESMO EMAIL DO USERNAME
+    $mail->setFrom('${{ secrets.USUARIO_EMAIL }}', 'Quote from Site');  //DEVE SER O MESMO EMAIL DO USERNAME
     $mail->addAddress('mvnigro@gmail.com');     // QUAL EMAIL RECEBERÁ A MENSAGEM!
     // $mail->addAddress('ellen@example.com');    // VOCÊ pode incluir quantos receptores quiser
     $mail->addReplyTo($email, $nome);  //AQUI SERA O EMAIL PARA O QUAL SERA RESPONDIDO                  
@@ -45,8 +43,10 @@ $mail = new PHPMailer\PHPMailer\PHPMailer();
     // $mail->addBCC('bcc@example.com'); //ADICIONANDO BCC
 
     // Attachments
-    $mail->AddAttachment($_FILES['photo']['tmp_name'], $_FILES['photo']['name']);         // Add attachments
-    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+    //$mail -> addAttachment($photo); //ADICIONANDO ANEXO
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
 
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
