43472be22861e3611d21cdb35bda453f8de508c7
mvnigro <mvnigro@gmail.com>
2022-06-18 23:06:07 -0300

    php

--- links/config.php
+++ links/config.php
@@ -1,10 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <? php 
-//create a form to get the user's name and email address
-echo '<form action="config.php" method="post">';
-echo '<p>Name: <input type="text" name="name" /></p>';
-echo '<p>Email: <input type="text" name="email" /></p>';
-echo '<p><input type="submit" /></p>';
-echo '</form>';
-
-
+//create a simple php page
+echo "Hello World";
 ?>
+
+</body>
+</html>
+
