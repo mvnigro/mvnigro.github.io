6f82625215892722d8c2617905f491d635f29bac
mvnigro <mvnigro@gmail.com>
2022-06-19 16:38:21 -0300

    email change

--- enviar.php
+++ enviar.php
@@ -27,16 +27,16 @@ $mail = new PHPMailer\PHPMailer\PHPMailer();
     $mail->SMTPDebug = 2;	
     $mail->Host       = 'smtp.hostinger.com';  // SEU HOST (HOSPEDAGEM)
     $mail->SMTPAuth   = true;                        // Manter em true
-    $mail->Username   = 'vicente@vnigro.net';   //SEU USUÁRIO DE EMAIL
-    $mail->Password   = 'Vicente000@@##';                   //SUA SENHA DO EMAIL SMTP password
+    $mail->Username   = 'site@vnigro.net';   //SEU USUÁRIO DE EMAIL
+    $mail->Password   = 'Vicente000@@$$';                   //SUA SENHA DO EMAIL SMTP password
     $mail->SMTPSecure = 'tls';    //TLS OU SSL-VERIFICAR COM A HOSPEDAGEM
     $mail->Port       = 587;     //TCP PORT, VERIFICAR COM A HOSPEDAGEM
     $mail->CharSet = 'UTF-8';
   
     
     //Recipients
-    $mail->setFrom('vicente@vnigro.net', 'Quote from Site');  //DEVE SER O MESMO EMAIL DO USERNAME
-    $mail->addAddress('vicente@vnigro.net');     // QUAL EMAIL RECEBERÁ A MENSAGEM!
+    $mail->setFrom('site@vnigro.net', 'Quote from Site');  //DEVE SER O MESMO EMAIL DO USERNAME
+    $mail->addAddress('site@vnigro.net');     // QUAL EMAIL RECEBERÁ A MENSAGEM!
     // $mail->addAddress('ellen@example.com');    // VOCÊ pode incluir quantos receptores quiser
     $mail->addReplyTo($email, $nome);  //AQUI SERA O EMAIL PARA O QUAL SERA RESPONDIDO                  
     // $mail->addCC('cc@example.com'); //ADICIONANDO CC
