f4e306cc44544c9cb79a909f5e7e3c233bc3b30f
mvnigro <mvnigro@gmail.com>
2022-06-18 21:57:23 -0300

    config

--- links/config.php
+++ links/config.php
@@ -1,12 +1,10 @@
-<? php
+<? php 
+//create a form to get the user's name and email address
+echo '<form action="config.php" method="post">';
+echo '<p>Name: <input type="text" name="name" /></p>';
+echo '<p>Email: <input type="text" name="email" /></p>';
+echo '<p><input type="submit" /></p>';
+echo '</form>';
 
-$db_host = 'localhost;
-$db_user = 'root';
-$db_pass = '';
-$db_name = 'links';
 
-$connection = new mysqli($db_host, $db_user, $db_pass, $db_name);
-if ($connection->connect_error) {
-    die('Connection failed: ' . $connection->connect_error);
-}
-?> 
+?>
